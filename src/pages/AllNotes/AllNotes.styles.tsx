05c3e10b474ee3ec1375b84b13f34a1a7e907c21
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-30 14:26:43 +0900

    feat: 전체페이지 정렬 버튼 추가

--- src/pages/AllNotes/AllNotes.styles.tsx
+++ src/pages/AllNotes/AllNotes.styles.tsx
@@ -1,5 +1,10 @@
 import { styled } from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Input = styled.input`
   width: 70vw;
   margin-left: 50px;
@@ -13,5 +18,27 @@ export const Input = styled.input`
   &: focus{
     outline: none;
     border-bottom: 3.5px solid #bdbdbd;
+  }
+`;
+
+export const SortingBtn = styled.button`
+  width: 90px;
+  height: 45px;
+  margin-right: 80px;
+  margin-left: auto;
+  margin-top: 15px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  border-radius: 10px;
+  border: 1px solid #bdbdbd;
+  cursor: pointer;
+  border-bottom: 2px solid #bdbdbd;
+  &: hover {
+    border-bottom: 3.5px solid #bdbdbd;
+  }
+  &:active {
+    box-shadow: 1px 1px 0 rgb(0,0,0,0.5);
+    position: relative;
+    top:2px;
   }
 `;
--- src/pages/AllNotes/AllNotes.tsx
+++ src/pages/AllNotes/AllNotes.tsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react'
-import { Input } from './AllNotes.styles'
+import React, { useEffect, useState } from 'react'
+import { Container, Input, SortingBtn } from './AllNotes.styles'
 
 const AllNotes = () => {
   const [search, setSearch] = useState('');
+  const [debouncedValue, setDebouncedValue] = useState(search);
+
   console.log(search);
+  console.log('debouncd: ', debouncedValue);
+
+  // 디바운스 커스텀훅으로도 나중에 리펙토링 해보자!
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(search);
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    }; //value 변경 시점에 clearTimeout을 해줘야함.
+  }, [search]);
 
   return (
-    <div>
+    <Container>
       {/* 검색 */}
       <Input placeholder='노트의 제목을 입력해주세요.' value={search} onChange={(e) => setSearch(e.target.value)}/> 
       {/* 정렬 */}
+      <SortingBtn>정렬</SortingBtn>
       {/* 노트 */}
       {/* pinned note */}
       {/* all notes */}
-    </div>
+    </Container>
   )
 }
 
