6ab83f66c4bde614742a0855e86f4a5042171489
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-30 12:43:11 +0900

    style: 사이드바 css 수정

--- src/layout/Sidebar/Sidebar.styles.tsx
+++ src/layout/Sidebar/Sidebar.styles.tsx
@@ -2,12 +2,23 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-color: rgb(254, 238,196);
-  width: 300px;
+  width: 20vw;
   height: 100vh;
 `;
 
-export const Title = styled.h1`
+export const TitleContainer = styled.div`
+  height: 60px;
+  // background-color: green;
+  border-bottom: 4px solid rgb(254,242,206);
+`;
 
+export const Title = styled.h1`
+  padding-left: 20px;
+  font-size: 32px;
+  font-weight: 700;
+  margin: 0px;
+  line-height: 60px;
+  
 `;
 
 export const MenuContainer = styled.div`
--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, MenuContainer, MenuText, Title } from './Sidebar.styles'
+import { Container, MenuContainer, MenuText, Title, TitleContainer } from './Sidebar.styles'
 import { FaLightbulb } from 'react-icons/fa';
 import { BiSolidPurchaseTag } from 'react-icons/bi';
 import { BiSolidBox } from 'react-icons/bi';
@@ -29,20 +29,22 @@ const Sidebar = () => {
   const navigate = useNavigate();
   return (
     <Container>
-      <Title>Keep</Title>
+      <TitleContainer>
+        <Title>Keep</Title>
+      </TitleContainer>
       <MenuContainer onClick={()=>navigate('/')}>
         <FaLightbulb style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Notes</MenuText>
       </MenuContainer>
-      <MenuContainer onClick={()=>navigate('/exercise')}>
+      {/* <MenuContainer onClick={()=>navigate('/exercise')}>
         <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Exercise</MenuText>
       </MenuContainer>
       <MenuContainer onClick={()=>navigate('/quotes')}>
         <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Quotes</MenuText>
-      </MenuContainer>
-      <MenuContainer onClick={()=>navigate('/tag1')}>
+      </MenuContainer> */}
+      <MenuContainer onClick={()=>navigate('/tag')}>
         <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>태그1</MenuText>
       </MenuContainer>
