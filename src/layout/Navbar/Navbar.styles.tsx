ad5ad0b8ecfab869f012fcefecdd1deba070f67a
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-30 12:43:57 +0900

    feat: 네비바 틀 작성

---
+++ src/layout/Navbar/Navbar.styles.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  // box-shadow: 0px 20px 15px -5px rgba(0,0,0,0.1);
+  border-bottom: 4px solid rgba(239, 239, 239);
+  // background-color: green;
+  width: 80vw;
+  height: 60px;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  line-height: 60px;
+  font-size: 1.5rem;
+  padding-left: 20px;
+`;
--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { Container, Title } from './Navbar.styles';
 
 const Navbar = () => {
+  // params 들고와서, 제일 상단에 보여주기.
+  // 노트 추가 버튼
+  const {pathname} = useLocation();
+  console.log(pathname);
+  console.log(pathname.slice(1,));
+  const title = pathname.slice(1,) === '' ? 'Notes' : pathname.slice(1,);
   return (
-    <div>Navbar</div>
+    <Container>
+      <Title>
+        {title}
+      </Title>
+    </Container>
   )
 }
 
