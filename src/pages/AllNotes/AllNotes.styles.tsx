5c4e0266c12db948e04ab4ff28e45bd2cb6c3f04
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-30 14:10:49 +0900

    add: 노트 검색 값 확인

--- src/pages/AllNotes/AllNotes.styles.tsx
+++ src/pages/AllNotes/AllNotes.styles.tsx
@@ -9,7 +9,7 @@ export const Input = styled.input`
   border-radius: 5px;
   border: 1px solid #bdbdbd;
   border-bottom: 2px solid #bdbdbd;
-  color: #bdbdbd;
+  // color: #bdbdbd;
   &: focus{
     outline: none;
     border-bottom: 3.5px solid #bdbdbd;
--- src/pages/AllNotes/AllNotes.tsx
+++ src/pages/AllNotes/AllNotes.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from './AllNotes.styles'
 
 const AllNotes = () => {
+  const [search, setSearch] = useState('');
+  console.log(search);
+
   return (
     <div>
       {/* 검색 */}
-      <Input placeholder='노트의 제목을 입력해주세요.'/>
+      <Input placeholder='노트의 제목을 입력해주세요.' value={search} onChange={(e) => setSearch(e.target.value)}/> 
       {/* 정렬 */}
       {/* 노트 */}
       {/* pinned note */}
