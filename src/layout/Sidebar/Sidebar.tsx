3f75e46606be43871e6a02e84405e94593fc0f67
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-31 16:37:08 +0900

    feat: 태그에 따라 사이드바 메뉴 추가

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -6,6 +6,8 @@ import { BiSolidBox } from 'react-icons/bi';
 import { FaTrash } from 'react-icons/fa6';
 import { BiSolidPencil } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 // const MenuItem = ({icon, text}: {icon: React.ReactNode, text: string}) => {
 //   console.log(icon);
@@ -24,9 +26,23 @@ import { useNavigate } from 'react-router-dom';
 //   // navigate(link);
 // }
 
+const TagItem = ({tagName}:{tagName: string}) => {
+  const navigate = useNavigate(); // 이거 중복 호출...?
+
+  return (
+    <>
+      <MenuContainer onClick={()=>navigate(`/tag/${tagName}`)}>
+        <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
+        <MenuText>{tagName}</MenuText>
+      </MenuContainer>
+    </>
+  )
+}
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const tagList = useSelector((state:RootState) => state.tag);
+  console.log(tagList);
   return (
     <Container>
       <TitleContainer>
@@ -36,6 +52,10 @@ const Sidebar = () => {
         <FaLightbulb style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Notes</MenuText>
       </MenuContainer>
+      {
+        tagList.map((tag) => <TagItem key={tag.id} tagName={tag.tag} />)
+      }
+      {/* 태그들 map 돌면서 출력 */}
       {/* <MenuContainer onClick={()=>navigate('/exercise')}>
         <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Exercise</MenuText>
@@ -44,10 +64,6 @@ const Sidebar = () => {
         <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Quotes</MenuText>
       </MenuContainer> */}
-      <MenuContainer onClick={()=>navigate('/tag')}>
-        <BiSolidPurchaseTag style={{fontSize: '30px', margin: 'auto 0'}} />
-        <MenuText>태그1</MenuText>
-      </MenuContainer>
       <MenuContainer onClick={()=>navigate('/edit')}>
         <BiSolidPencil style={{fontSize: '30px', margin: 'auto 0'}} />
         <MenuText>Edit Notes</MenuText>
