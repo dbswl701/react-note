b2c5198f18b3ffd6fbe2f8fa1fb20033abc3c393
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-31 12:34:02 +0900

    add: 태그 입력받아 추가하는 기능 추가

--- src/components/ModalAddTag/ModalAddTag.tsx
+++ src/components/ModalAddTag/ModalAddTag.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Input, TagContainer, TagListContainer, TagTitle, Title } from './ModalAddTag.styles'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../store'
 import { TagType } from '../../types/tag'
 import { FaPlus } from 'react-icons/fa6';
+import { addTag } from '../../store/tagSlice.'
 // import { FaMinus } from 'react-icons/fa6';
 
 const TagItem = ({tag}: {tag: TagType}) => {
@@ -20,11 +21,24 @@ const TagItem = ({tag}: {tag: TagType}) => {
 
 const ModalAddTag = () => {
   const tagList = useSelector((state:RootState) => state.tag);
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+  console.log(text);
   console.log(tagList);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // tag에 추가
+    dispatch(addTag({id: 5, tag: text}));
+    setText('');
+  }
+
   return (
     <Container>
       <Title>ADD Tags</Title>
-      <Input placeholder='new tag...'/>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <Input placeholder='new tag...' value={text} onChange={(e) => setText(e.target.value)}/>
+      </form>
       <TagListContainer>
         {
           tagList?.map((tag) => <TagItem key={tag.id} tag={tag} />)
