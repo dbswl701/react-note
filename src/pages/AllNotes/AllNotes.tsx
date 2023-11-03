ac394e47549685af61a3314ed7f36907d131603b
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-03 11:50:51 +0900

    feat: 검색 기능 구현

--- src/pages/AllNotes/AllNotes.tsx
+++ src/pages/AllNotes/AllNotes.tsx
@@ -7,26 +7,36 @@ import NoteWrapper from '../../components/NoteWrapper/NoteWrapper';
 
 const AllNotes = () => {
   const [search, setSearch] = useState('');
-  const [debouncedValue, setDebouncedValue] = useState(search);
-
   const notes = useSelector((state: RootState) => state.noteList);
-  console.log(notes);
 
-  const filteredNotesList = notes.filter(note => note.type === 'normal');
+  const [debouncedValue, setDebouncedValue] = useState(search);
+  const [AllNotesList, setAllNotesList] = useState(notes.filter(note => note.type === 'normal'));
+
+  const [filteredNotesList, setFilteredNotesList] = useState([...AllNotesList]);
+  console.log(notes);
 
   console.log(search);
   console.log('debouncd: ', debouncedValue);
+
+  useEffect(() => {
+    setAllNotesList(notes.filter(note => note.type === 'normal'));
+  }, [notes]);
 
   // 디바운스 커스텀훅으로도 나중에 리펙토링 해보자!
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedValue(search);
-    }, 200);
+    }, 500);
 
     return () => {
       clearTimeout(timer);
     }; //value 변경 시점에 clearTimeout을 해줘야함.
   }, [search]);
+
+  // 디바운스로 값 바뀔 때 마다 검색
+  useEffect(() => {
+    setFilteredNotesList(AllNotesList.filter((note) => note.name.includes(debouncedValue) || note.content.includes(debouncedValue)));
+  }, [debouncedValue, AllNotesList])
 
   const pinnedNoteList = filteredNotesList.filter(note => note.pinned);
   const allNoteList = filteredNotesList.filter(note => !note.pinned);
